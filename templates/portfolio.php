e128e061d6f09994a65ce1722bf0b8432499eec1
Charisse <charisse.ysabel930@gmail.com>
2015-10-31 15:09:28 +0100

    Add basic features

---
+++ templates/portfolio.php
@@ -0,0 +1,16 @@
+<div>
+    <img alt="Under Construction" src="/img/construction.gif"/>
+</div>
+<div>
+    <a href="logout.php">Log Out</a>
+</div>
+
+
+<!--
+	first page rendered by INDEX.PHP (because there's no other pages included in index.php)
+	if log out is clicked, page will go to LOGOUT.PHP
+
+
+
+	LOGOUT.PHP: 
+-->
