fef6414b7c554e02b6f6ca790d989df393a83916
Charisse <charisse.ysabel930@gmail.com>
2015-11-01 19:34:50 +0100

    Add table

--- templates/header.php
+++ templates/header.php
@@ -6,7 +6,9 @@
 
         <link href="/css/bootstrap.min.css" rel="stylesheet"/>
         <link href="/css/bootstrap-theme.min.css" rel="stylesheet"/>
+        <link href="/css/portfolio-style.css" rel="stylesheet"/>
         <link href="/css/styles.css" rel="stylesheet"/>
+
 
         <?php if (isset($title)): ?>
             <title>C$50 Finance: <?= htmlspecialchars($title) ?></title>
--- templates/portfolio.php
+++ templates/portfolio.php
@@ -1,3 +1,5 @@
+<!--
+
 <div>
     <img alt="Under Construction" src="/img/construction.gif"/>
 </div>
@@ -5,12 +7,44 @@
     <a href="logout.php">Log Out</a>
 </div>
 
-
-<!--
-	first page rendered by INDEX.PHP (because there's no other pages included in index.php)
-	if log out is clicked, page will go to LOGOUT.PHP
-
-
-
-	LOGOUT.PHP: 
 -->
+
+<!-- TABLE -->
+<table class="table table-striped">
+	<thead>
+		<tr>
+			<th> <strong>Name</strong> </th>
+			<th> <strong>Symbol</strong> </th>
+			<th> <strong>Shares</strong> </th>
+			<th> <strong>Price</strong> </th>			
+			<th> <strong>TOTAL</strong> </th>
+		</tr>
+	</thead>
+	
+	<tbody>
+		<?php foreach($positions as $position): ?>
+		<tr>
+			<td><?= $position["name"]	?></td>
+			<td><?= $position["symbol"]	?></td>		
+			<td><?= $position["shares"]	?></td>
+			<td>$ <?= $position["price"]	?> </td>
+			<td>$ <?= $position["total"] ?></td>
+
+		</tr>
+		<?php endforeach ?>
+		<tr id="cash">
+			<td colspan="4">CASH</td>
+			<td>$ <?= htmlspecialchars($cash)?> </td>
+		</tr>
+		
+	</tbody>
+</table>
+
+
+
+
+
+
+
+
+
