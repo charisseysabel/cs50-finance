3ed0bb4ada54ea72fc28f20df6293bfe3f52fd14
Charisse <charisse.ysabel930@gmail.com>
2015-11-04 17:26:17 +0100

    Update header

--- templates/header.php
+++ templates/header.php
@@ -34,7 +34,7 @@
                 	<li> <a href="quote.php">Quote</a> </li>
                 	<li> <a href="buy.php">Buy</a> </li>
                 	<li> <a href="sell.php">Sell</a> </li>
-                	<li> <a href="/">History</a> </li>
+                	<li> <a href="history.php">History</a> </li>
                 	<li> <a href="logout.php">Log Out</a> </li>
                 </ul>
             </div>
